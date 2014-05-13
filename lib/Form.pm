c79e5494a15f4fdfc8c5da8eb416a1f319bd0b4c
Nikonov Alexey <NikonovAlexey@inbox.ru>
2014-05-13 13:36:45 +0400

    Новый плагин для обработки форм.

---
+++ lib/Form.pm
@@ -0,0 +1,35 @@
+package Dancer::Plugin::Form;
+
+use strict;
+use warnings;
+
+use Dancer ':syntax';
+use Dancer::Plugin;
+use Dancer::Plugin::DBIC;
+use Dancer::Plugin::Common;
+
+use Dancer::Engine;
+
+use FAW::uRoles;
+use Data::Dump qw(dump);
+use FindBin qw($Bin);
+use Try::Tiny;
+use POSIX 'strftime';
+
+sub foobar {
+    my $item
+    warning " ============ $item";
+    #$engine->render('');
+}
+
+=text
+
+Регистрация процедур плагина
+
+=cut
+
+register foobar => \&foobar;
+
+register_plugin;
+
+1;
